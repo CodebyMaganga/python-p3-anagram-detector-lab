ae783a7bf3197376fedeb881ae74e09b0fc191a6
Maganga Mwambonu <codebymaganga@gmail.com>
2023-12-01 21:22:24 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word.lower()
+    
+    def match(self, source_list):
+        new_list = list()
+        x = sorted(self.word)
+        for letter in x:
+            new_list.append(letter)
+        matched_anagrams = []
+        for name in source_list:
+            if sorted(name.lower()) == x and name.lower() != self.word:
+                matched_anagrams.append(name)
+        return matched_anagrams
+                
+                
+                
